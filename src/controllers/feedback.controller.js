c96fc9a82e71cfecc1552ce0c23041769f45abb9
Timo <timofey.khmelev@gmail.com>
2025-10-05 11:03:56 +0500

    feedback controller added

--- src/controllers/feedback.controller.js
+++ src/controllers/feedback.controller.js
@@ -12,11 +12,16 @@ export const sendFeedback = async (req, res) => {
 
 	const project = 'money-log';
 
-	await Feedback.create({
-		message,
-		user_id: userId,
-		project
-	});
+	try {
+		await Feedback.create({
+			message,
+			user_id: userId,
+			project
+		});
+	} catch (error) {
+		console.error('Error saving feedback to database:', error);
+	}
+
 
 	await telegramService.sendMessage('Feedback sent '+message);
 
