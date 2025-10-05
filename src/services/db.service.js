a83044005107edc4f02bd17f01c4ff94842f5eb7
Timo <timofey.khmelev@gmail.com>
2025-10-05 10:50:12 +0500

    feedback controller added

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -1,9 +1,13 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('uty', 'root', 'Ingai4ch', {
-	host: '127.0.0.1',
-	dialect: 'mysql',
-	logging: false, // set true to see SQL logs
-});
+const sequelize = new Sequelize(
+		process.env.DB_DATABASE,
+		process.env.DB_USERNAME,
+		process.env.DB_PASSWORD,
+		{
+			host: '127.0.0.1',
+			dialect: 'mysql',
+			logging: false, // set true to see SQL logs
+		});
 
 export default sequelize;
