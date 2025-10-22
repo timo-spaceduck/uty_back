5f226bb2a49a262f2dd8224c11ccafb29513f946
Timo <timofey.khmelev@gmail.com>
2025-10-22 23:45:02 +0500

    use env for bearer token

--- src/services/notifican.service.js
+++ src/services/notifican.service.js
@@ -2,13 +2,12 @@ import axios from 'axios';
 
 export const sendToNotifican = async (message) => {
 	try {
-		// await axios.post('https://api.notifican.com/5f0579b9-3d9c-4ec9-a2f9-732868e4bf00', {
-		await axios.post('https://api.notifican.com/063ca464-09f2-4a86-82f4-478c83234fad', {
+		await axios.post(`https://api.notifican.com/${process.env.NOTIFICAN_USER_ID}`, {
 			categoryId: 10,
 			message,
 		}, {
 			headers: {
-				'Authorization': 'Bearer fc8ac7bb-bb35-4a66-b400-398fdba3852c'
+				'Authorization': `Bearer ${process.env.NOTIFICAN_BEARER_TOKEN}`
 			}
 		})
 	} catch (e) {
