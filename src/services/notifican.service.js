c9baea67910e929dd100f4618fcf4fe851e79e4e
Timo <timofey.khmelev@gmail.com>
2025-10-22 23:06:44 +0500

    notifican updated

--- src/services/notifican.service.js
+++ src/services/notifican.service.js
@@ -2,8 +2,14 @@ import axios from 'axios';
 
 export const sendToNotifican = async (message) => {
 	try {
-		await axios.post('https://api.notifican.com/5f0579b9-3d9c-4ec9-a2f9-732868e4bf00', {
-			message
+		// await axios.post('https://api.notifican.com/5f0579b9-3d9c-4ec9-a2f9-732868e4bf00', {
+		await axios.post('https://api.notifican.com/063ca464-09f2-4a86-82f4-478c83234fad', {
+			categoryId: 10,
+			message,
+		}, {
+			headers: {
+				'Authorization': 'Bearer fc8ac7bb-bb35-4a66-b400-398fdba3852c'
+			}
 		})
 	} catch (e) {
 		console.log(e);
