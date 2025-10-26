858bdd29a34c95a92d3a397674bf2ac59e5af2ee
Timo <timofey.khmelev@gmail.com>
2025-10-26 12:51:35 +0500

    added notifican

--- src/services/cors.service.js
+++ src/services/cors.service.js
@@ -4,12 +4,15 @@ const allowedOrigins = [
 	'http://localhost:3000',
 	'http://localhost:3259',
 	'http://localhost:3399',
+	'http://localhost:3369',
 	'https://uty.lol',
 	'https://www.uty.lol',
 	'https://timo-dev.com',
 	'https://www.timo-dev.com',
 	'https://money-log.app',
 	'https://www.money-log.app',
+	'https://notifican.com',
+	'https://www.notifican.com',
 ];
 
 const corsOptions = {
